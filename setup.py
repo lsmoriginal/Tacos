253223cc900017e796ebd05d3ac37efbbbfedef9
lsmoriginal <38485330+lsmoriginal@users.noreply.github.com>
2022-01-26 22:17:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Toolbox",
+    name="Tacos",
     version="0.0.1",
     author="ShaoMin Liu",
     author_email="liushaomin2@gmail.com",
     description="Simple wrappers for some commonly use functionalities",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lsmoriginal/Toolbox",
+    url="https://github.com/lsmoriginal/Tacos",
     packages=setuptools.find_packages(),
     package_data={
         '':['*.json', '*.csv', "*.dic", "*.aff"]},
@@ -22,9 +22,9 @@ setuptools.setup(
     ],
     install_requires=[
         "Pillow<=8.3.1",
+        "Pandas"
         # "pynvml",
         # "psutil",
-        
     ],
     python_requires='>=3.6'
 )
